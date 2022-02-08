0443344b543e0a4a61911375d32b049a327e452b
Mateo <mateog91@gmail.com>
2022-02-08 01:03:07 -0500

    FIXES

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -37,14 +37,15 @@ def delete_states(state_id):
     return jsonify({}), 200
 
 
-@app_views.route('/states/<state_id>', methods=['POST'], strict_slashes=False)
-def post_states(state_id):
+@app_views.route('/states/', methods=['POST'], strict_slashes=False)
+def post_states():
     state_jason = request.get_json(silent=True)
     if not state_jason:
         return(jsonify({'error': 'Not a JSON'})), 400
     if 'name' not in state_jason:
         return(jsonify({'error': 'Missing name'})), 400
     state = State(**state_jason)
+    storage.new(state)
     state.save()
     return jsonify(state.to_dict()), 201
 
@@ -52,10 +53,15 @@ def post_states(state_id):
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def put_states(state_id):
     state_json = request.get_json(silent=True)
+    if not state_id:
+        abort(404)
     if not state_json:
         return(jsonify({'error': 'Not a JSON'})), 400
-    if 'name' not in state_json:
-        return(jsonify({'error': 'Missing name'})), 400
-    state = State(**state_json)
+    state = storage.get(State, state_id)
+    for key, value in state_json.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(state, key, value)
+    storage.new(state)
     state.save()
-    return jsonify(state.to_dict()), 201
+
+    return jsonify(state.to_dict()), 200
